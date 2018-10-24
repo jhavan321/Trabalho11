055f916d54148fd3ed11a899730b4f75e987b02e
Christian <christian.201810338@unilasalle.edu.br>
2018-10-24 20:59:42 -0300

    biblioteca arquivo em main

--- main.c
+++ main.c
@@ -1,38 +1,37 @@
 #include <stdio.h>
+#include <strings.h>
 #include "bibliotecas/telas.h"
+#include "bibliotecas/arquivo.h"
 
-void main(){
-int opcao;
-struct filme f;
-
-do {
-
-opcao = home();
-switch (opcao){
-case 1: 
-cadastrar(f);
-break;
-case 2:
-editar(f);
-break;
-case 3: 
-locar(f);
-break;
-case 4:
-devolver(f);
-break;
-case 5:
-break;
-case 6:
-break;
-case 7:
-break;
-} 
-
-}
-
-while (opcao !=8);
-
-
-
+int main(){
+	int opcao;
+	struct filme f[50];
+	ler(f);
+	printf("%s\n", f[0].nome);
+	return 0;
+	do {
+		opcao = home();
+		switch (opcao){
+			case 1: 
+				//cadastrar(f);
+				break;
+			case 2:
+				//editar(f);
+				break;
+			case 3: 
+				//locar(f);
+				break;
+			case 4:
+				//devolver(f);
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			case 7:
+				break;
+		}
+	}
+	while (opcao !=8);
+	return 0;
 }
